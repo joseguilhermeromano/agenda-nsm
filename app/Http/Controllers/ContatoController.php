95ec27bd281f6cea688e0f42f844b30f5ae21eae
José Guilherme Romano <jose_guilherme_romano@hotmail.com>
2021-01-14 00:31:37 -0300

    configuracao e layout para envio de e-mails.

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -9,10 +9,10 @@ use Mail;
 
 class ContatoController extends Controller
 {
-    protected $contatos;
+    protected $contato;
 
     public function __construct(){
-        $this->contatos = new Contato();
+        $this->contato = new Contato();
     }
     /**
      * Show the profile for a given user.
@@ -43,26 +43,51 @@ class ContatoController extends Controller
 
         $fullFileName = public_path('uploads')."\\".$fileName;
 
-        $this->contatos->nome = $request->nome;
-        $this->contatos->email = $request->email;
-        $this->contatos->mensagem = $request->mensagem;
-        $this->contatos->telefone = preg_replace('/\D/', '', $request->telefone);
-        $this->contatos->arquivo = $fullFileName;
-        $this->contatos->ip = $request->ip();
-        $this->contatos->save();
+        $this->contato->nome = $request->nome;
+        $this->contato->email = $request->email;
+        $this->contato->mensagem = $request->mensagem;
+        $this->contato->telefone = preg_replace('/\D/', '', $request->telefone);
+        $this->contato->arquivo = $fullFileName;
+        $this->contato->ip = $request->ip();
+        $this->contato->save();
+
+        $this->contato->where('email', $this->contato->email)->get();
+        $data_envio = date("d/m/Y", strtotime($this->contato->created_at));
    
-        return $this->enviarEmail();
+        return $this->enviarEmail($data_envio);
     }
 
-    public function enviarEmail(){
+    public function enviarEmail($data_envio){
 
-        $detalhes["titulo"] = "Primeiro e-mail de teste com laravel";
-        $detalhes["corpo"] = "teste.";
+        $detalhes["titulo"] = "Novo Contato";
+
+        $detalhes["corpo"] = "
+        <h3>Nome: {$this->contato->nome} </h3>
+        <br>
+        <p>
+        <b>E-mail:</b> {$this->contato->email} <br>
+        <b>Mensagem: </b> {$this->contato->mensagem} <br>
+        <b>Telefone: </b> {$this->formataTelefone($this->contato->telefone)} <br>
+        <b>IP:</b> {$this->contato->ip} <br>
+        <b>Data envio: </b> {$data_envio}
+        </p>
+        ";
 
         \Mail::to('agenda.netshowsme@gmail.com')->send(new \App\Mail\SendMail($detalhes));
 
         return back()
             ->with('success','Todas as informações foram enviadas com sucesso!');
+    }
+
+    protected function formataTelefone($telefone){
+        $formatado = preg_replace('/[^0-9]/', '', $telefone);
+        $matches = [];
+        preg_match('/^([0-9]{2})([0-9]{4,5})([0-9]{4})$/', $formatado, $matches);
+        if ($matches) {
+            return '('.$matches[1].') '.$matches[2].'-'.$matches[3];
+        }
+
+        return $telefone; 
     }
 
 }
